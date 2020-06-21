adfff0f70ac815efdc3668c7bc5051f437032984
Даниил Стефаненков <daniilstef2391@gmail.com>
2020-06-21 14:38:15 +0300

    Добавьте файлы проекта.

---
+++ pract7_ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract7_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            string result;
+            Console.WriteLine(" Напишите строку: ");
+            str = Console.ReadLine();
+            if (str.Length % 2 != 0) 
+                result = str.Remove((str.Length / 2), 1); 
+            else
+                result = str.Remove((str.Length / 2) - 1, 2);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
